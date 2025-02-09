02a9749bdd3f91b20bc25c5535bd2d8daf330045
Chanakan Prayoonphong <ccnope@proton.me>
2025-02-09 15:26:17 +0700

    update: 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -13,5 +15,14 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true,
+    cacheOnDemandPages: true,
+  }),
+  image: {
+    domains: ['cdn.discordapp.com'],
+  },
 });
